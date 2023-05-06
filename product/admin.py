3249a8c19770a05d59844f8afe00f62011669be9
zora89 <zorawar.purohit@gmail.com>
2023-05-06 11:42:22 +0530

    first commit

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Category, Tag, Manufacturer, Product
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(Manufacturer)
+admin.site.register(Product)
